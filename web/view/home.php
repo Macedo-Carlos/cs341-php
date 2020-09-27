67edc957c67f74027e145c56837dbfb92f8e5c5c
Macedo-Carlos <35361999+Macedo-Carlos@users.noreply.github.com>
2020-09-27 16:26:04 -0600

    I changed the wording of the introductions to make it more compelling

--- web/view/home.php
+++ web/view/home.php
@@ -15,7 +15,7 @@
         <section class="introductionSection">
         <h2>Introduction</h2>
         <p>When I was very young, one day my mom came to me with some paper and she folded one piece into a flapping bird and then she folded another piece into a jumping frog. Then she gave me an origami book and she told me that I could make any toy I wanted out of paper; her words blew my mind. I've been folding paper into origami ever since and I love it.</p>
-        <p>I think coding can be a lot like origami in a lot of ways and that's why I like it too. Take a look at my projects for this semester.</p>
+        <p>I think coding can be like origami in a lot of ways. There is a special language to communicate instructions that need to be followed in order to make an origami model. That's why I like coding too. Take a look at my projects for this semester.</p>
         <a class="regularButton" href="project-list.html">CSE 341 Projects</a>
         </section>
         <section class="imagesSection">
@@ -31,7 +31,7 @@
             <img onclick="modalOnClick(this)" class="modalImage" src="images/origami-models/origami-rose.JPG" alt="Rose">
             <img onclick="modalOnClick(this)" class="modalImage" src="images/origami-models/origami-koi.JPG" alt="Koi Fish">
             <img onclick="modalOnClick(this)" class="modalImage" src="images/origami-models/origami-koi-fish.JPG" alt="Koi Fish Framed">
-            <img onclick="modalOnClick(this)" class="modalImage" src="images/origami-models/origami-snoopy.jpg" alt="Snoopy Framed">
+            <img onclick="modalOnClick(this)" class="modalImage" src="images/origami-models/origami-snoopy.JPG" alt="Snoopy Framed">
             <img onclick="modalOnClick(this)" class="modalImage" src="images/origami-models/origami-panda.JPG" alt="Panda">
             <img onclick="modalOnClick(this)" class="modalImage" src="images/origami-models/origami-bouque.JPG" alt="Floran Arrangement">
         </section>
