37f70802fef062c09df517469862bfc4f5918358
Macedo-Carlos <35361999+Macedo-Carlos@users.noreply.github.com>
2020-10-20 21:13:46 -0600

    sql corrections

--- web/team-projects/06Teach/db_update.php
+++ web/team-projects/06Teach/db_update.php
@@ -7,22 +7,18 @@ if (count($_POST)){
     $content = $_POST['content'];
     $topics = json_decode($_POST['topics']);
     $db = herokuConnect();
-    $sql = "INSERT INTO scriptures(book,chapter,verseFrom,content) VALUES ($book, $chapter, $verse, $content)";
+    $sql = "INSERT INTO scriptures (book, chapter, verseFrom, content) VALUES ('$book', '$chapter', '$verse', '$content')";
     $stmt = $db->prepare($sql);
     $stmt->execute();
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     foreach ($topics as $topicId) {
-        $sql = "INSERT INTO scripttopics (topic_id, script_id) VALUES ($row[id], $topicId)";
+        $sql = "INSERT INTO scripttopics (topic_id, script_id) VALUES ('$row[id]', '$topicId')";
         $stmt = $db->prepare($sql);
         $stmt->execute();
     }
+    $stmt->closeCursor();
     print "Success!";
 }
 
 ?>
-
-
-foeach check numbers
-    if SQL select from the topics to get ID if it exists insert this with scripture ID into scripttopics
-    else insert this new topic into topics get the row back from success and assign the id to the new insert into scripttopics
     
