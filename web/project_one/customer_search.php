763c7c290d629ac161989ece3d5bc0c3d13e5501
Macedo-Carlos <35361999+Macedo-Carlos@users.noreply.github.com>
2020-10-30 01:01:41 -0600

    fasdfasdf

--- web/project_one/customer_search.php
+++ web/project_one/customer_search.php
@@ -1,17 +1,22 @@
 <?php
+session_start();
 require_once 'library/connections.php';
 require_once 'library/functions.php';
 require_once 'model/orders_model.php';
+$_SESSION['testVar1'] = 'Ready';
 
   // Filter and store the data
   $customerName = filter_input(INPUT_POST, 'customerName', FILTER_SANITIZE_STRING);
+  $_SESSION['customerName'] = $customerName;
   // Check for missing data
   if(empty($customername)){
     $message = "Please provide valid information for all empty form fields.";
     exit; 
   }
   $customerSearch = searchByCustomerName($customerName);
+  $_SESSION['customerSearch'] = $customerSearch;
   $customerList = getCustomersList($customerSearch);
+  $_SESSION['customerList'] = $customerList;
   echo $customerList;
 
 ?>
--- web/project_one/test_session.php
+++ web/project_one/test_session.php
@@ -3,6 +3,9 @@ session_start();
 
 echo 'Test session here';
 
-echo $_SESSION['testvar1'];
+echo $_SESSION['testVar1'];
+echo $_SESSION['customerName'] = $customerName;
+echo $_SESSION['customerSearch'] = $customerSearch;
+echo $_SESSION['customerList'] = $customerList;
 
 ?>
