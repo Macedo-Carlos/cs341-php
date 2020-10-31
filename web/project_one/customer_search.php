0a5580c1aa37169a1ea921c3eea4e335e1bfc2f4
Macedo-Carlos <35361999+Macedo-Carlos@users.noreply.github.com>
2020-10-30 23:22:09 -0600

    adsfasdfasd

--- web/project_one/customer_search.php
+++ web/project_one/customer_search.php
@@ -1,20 +1,31 @@
 <?php
-session_start();
 require_once 'library/connections.php';
 require_once 'library/functions.php';
 require_once 'model/orders_model.php';
-$_SESSION['testVar1'] = 'Ready';
 
 // Filter and store the data
-$customerName = filter_input(INPUT_POST, 'customerName', FILTER_SANITIZE_STRING);
+//$customerName = filter_input(INPUT_POST, 'customerName', FILTER_SANITIZE_STRING);
 
-// Check for missing data
+/* // Check for missing data
 if(empty($customername)){
   $message = "Please provide valid information for all empty form fields.";
   exit; 
-}
+} */
+$customerName = "ry";
+
 $customerSearch = searchByCustomerName($customerName);
 $customerList = getCustomersList($customerSearch);
-echo $customerList;
+
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Customer Search</title>
+</head>
+<body>
+  <div id="outputDiv">echo $customerList;</div>
+</body>
+</html>
